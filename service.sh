f22bf6f6185bfdca029f7ed1e9db4b873ec02926
Luca <luca.ant96@libero.it>
2020-08-29 23:03:46 +0200

    update pip

--- service.sh
+++ service.sh
@@ -12,14 +12,14 @@ OPTION=""
 
 if [ $# == 1 ]
 then
-	OPTION="$1"
+    OPTION="$1"
 fi
 
 
 if [ $OPTION == "run" ]
 then
-	source "$VENV_ACT"
-	python3 "$CURRENT_DIR"/busbolobot.py
+    source "$VENV_ACT"
+    python3 "$CURRENT_DIR"/busbolobot.py
 
 fi
 
@@ -27,34 +27,33 @@ fi
 if [ $OPTION == "install" ] 
 then
 
-	
-	
 
-	python3 -m venv "$CURRENT_DIR"/venv
-	source "$CURRENT_DIR"/venv/bin/activate
+    python3 -m venv "$CURRENT_DIR"/venv
+    source "$CURRENT_DIR"/venv/bin/activate
 
-	python3 -m pip install -r "$CURRENT_DIR/"requirements.txt
-	deactivate
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r "$CURRENT_DIR/"requirements.txt
+    deactivate
 
 
-	BOT_USER=$(whoami)
-	BUS_BOT_TOKEN="YOUR_TOKEN_HERE"
-	
-	echo -e "[Unit]
-Description=Bus Bologna bot
-After=network.target
-StartLimitIntervalSec=0
-[Service]
-Environment=BUS_BOT_TOKEN=$BUS_BOT_TOKEN
-WorkingDirectory="$CURRENT_DIR"
-Type=simple
-Restart=always
-RestartSec=1
-User=$BOT_USER
-ExecStart="$CURRENT_DIR/service.sh run"
+    BOT_USER=$(whoami)
+    BUS_BOT_TOKEN="YOUR_TOKEN_HERE"
 
-[Install]
-WantedBy=multi-user.target" | sudo tee /etc/systemd/system/busbolobot.service
+    echo -e "[Unit]
+    Description=Bus Bologna bot
+    After=network.target
+    StartLimitIntervalSec=0
+    [Service]
+    Environment=BUS_BOT_TOKEN=$BUS_BOT_TOKEN
+    WorkingDirectory="$CURRENT_DIR"
+    Type=simple
+    Restart=always
+    RestartSec=1
+    User=$BOT_USER
+    ExecStart="$CURRENT_DIR/service.sh run"
+
+    [Install]
+    WantedBy=multi-user.target" | sudo tee /etc/systemd/system/busbolobot.service
 
 
 fi
